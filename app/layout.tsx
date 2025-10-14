311f1aebcb32f6b5a7120fa1348a8acbf7c7eb03
Shayan <shayan.cs11@gmail.com>
2025-10-14 13:08:03 +0500

    fix: nextjs suspense and useParams issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { FeedbackForm } from "@/components/ui/feedback-form";
 import { Toaster } from "@/components/ui/sonner";
 import { GoogleAnalytics } from '@next/third-parties/google'
-// app/layout.tsx
 import HotjarProvider from "./providers/HotjarProvider";
+import { Suspense } from "react";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${dmSans.variable} antialiased`}>
       <body className="font-sans">
-        <HotjarProvider />
+         <Suspense fallback={null}><HotjarProvider /></Suspense>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
